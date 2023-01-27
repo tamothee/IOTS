aa227b67631b01e0bd6f2373cbc6f1f90680a0a1
tamothee <timo9132@gmail.com>
2023-01-27 13:19:33 +0800

    forgot to import nextlink

--- IOTS-dashboard/dashboard/app/page.js
+++ IOTS-dashboard/dashboard/app/page.js
@@ -11,6 +11,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 
