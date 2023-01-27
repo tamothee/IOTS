ecd6287238808ff4e3e2648bc4562732f8825588
tamothee <timo9132@gmail.com>
2023-01-27 13:17:42 +0800

    retrieve users devices

--- IOTS-dashboard/dashboard/app/page.js
+++ IOTS-dashboard/dashboard/app/page.js
@@ -12,17 +12,34 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-
 // Create the Application
 
 // Define the App component
 
 const HomePage = () => {
   // Set state variables
-  
+
   // This useEffect hook will run only once when the page is loaded and when
   // mongodb connection is established
   const { mongodb, user, permission, app } = useContext(mongodbContext);
+  const [devices, setDevices] = useState();
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        //connect to database
+        const collection = mongodb.db("IOTS_dashboard").collection("iot"); // Everytime a change happens in the stream, add it to the list of events
+        const devices = collection.find({});
+        setDevices(devices)
+      } catch (err) {
+        console.error("Failed to log in", err.message);
+      }
+    };
+    if (mongodb) {
+      //dont run watch when mongodb connection is not established
+      getUserData();
+    }
+  }, [mongodb]);
 
   function write() {
     if (mongodb) {
@@ -31,45 +48,45 @@ const HomePage = () => {
       collection.insertOne({
         timestamp: new Date(),
         password: 1234567,
-        owner_id: user.id
+        owner_id: user.id,
       });
     } else {
       alert("Mongodb connection not established. Please try again");
     }
   }
- 
 
   // Return the JSX that will generate HTML for the page
   return (
     <div>
-        homepage
+      <Breadcrumbs aria-label="breadcrumb" style={{ marginBottom: "10px" }}>
+        <Link
+          style={{ color: "inherit" }}
+          underline="hover"
+          color="inherit"
+          href="/"
+        >
+          Home
+        </Link>
+      </Breadcrumbs>
+      <div>
+        {!!user && //check if user is loaded
+          devices?.map((device) => {
+            return (
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography variant="h5" gutterBottom>
+                    {device.id}
+                  </Typography>
+                  <Typography variant="body2">
+                    Last updated at: {device.timestamp}
+                  </Typography>
+                </CardContent>
+              </Card>
+            );
+          })}
         <button onClick={write}>write</button>
+      </div>
     </div>
-    // <div className="App">
-    //   {!!user  && ( //check if user is loaded
-    //     <Card sx={{ minWidth: 275 }}>
-    //       <CardContent>
-    //         <Typography variant="h5" gutterBottom>
-    //           Number Of People
-    //         </Typography>
-    //         <Typography variant="body2">
-    //           {!!events ? (
-    //             <div>
-    //               {events.fullDocument.value}
-    //               <br />
-    //               {Date(JSON.stringify(events.fullDocument.timestamp))}
-    //             </div>
-    //           ) : (
-    //             "Waiting for update"
-    //           )}
-    //           {/* {events?.fullDocument.timestamp} */}
-    //           {console.log(events)}
-    //         </Typography>
-    //       </CardContent>
-    //     </Card>
-    //   )}
-    //   <button onClick={write}>write</button>
-    // </div>
   );
 };
 
